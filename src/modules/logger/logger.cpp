b60f3b6fdb724adf5ccf49820112e2264f482564
valli28 <valli28@hotmail.com>
2018-03-15 15:16:30 +0100

    Magwick Attitude Filter implemented and converted into roll pitch and yaw

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -634,7 +634,7 @@ void Logger::add_default_topics()
 	add_topic("vehicle_vision_position");
 	add_topic("vtol_vehicle_status", 200);
 	add_topic("wind_estimate", 200);
-	add_topic("extended_kalman_pos", 200);
+	add_topic("extended_kalman", 200);
 }
 
 void Logger::add_high_rate_topics()
